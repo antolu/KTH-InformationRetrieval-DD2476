28abc7d64243ebfd1c34f7c70fa7f1a9208c6bd0
Anton Lu <anton.lu@epfl.ch>
2019-01-21 16:19:07 +0100

    Add windows executables

---
+++ scripts/run_tokenizer.bat
@@ -0,0 +1,4 @@
+javac -cp . -d build ir\TokenTest.java ir\Tokenizer.java
+cd build
+java -cp . ir.TokenTest -f token_test.txt -p ..\patterns.txt -rp -cf > ..\tokenized_result.txt
+fc ..\token_test_tokenized_ok.txt ..\tokenized_result.txt
