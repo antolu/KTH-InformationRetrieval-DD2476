04fee91b8669b8a6e3913520bfa16cafb07cea02
Anton Ly <anton96lu@gmail.com>
2019-01-21 17:10:11 +0100

    Updated directories

--- scripts/compile_all.bat
+++ scripts/compile_all.bat
@@ -1,2 +1,2 @@
-if not exist classes mkdir classes
-javac -cp . -d classes ir/Engine.java ir/Tokenizer.java ir/TokenTest.java ir/Index.java ir/Indexer.java ir/Searcher.java ir/HashedIndex.java ir/PersistentHashedIndex.java ir/Query.java ir/QueryType.java ir/RankingType.java ir/PostingsList.java ir/PostingsEntry.java ir/SearchGUI.java
+if not exist build mkdir classes
+javac -cp . -d build ir/Engine.java ir/Tokenizer.java ir/TokenTest.java ir/Index.java ir/Indexer.java ir/Searcher.java ir/HashedIndex.java ir/PersistentHashedIndex.java ir/Query.java ir/QueryType.java ir/RankingType.java ir/PostingsList.java ir/PostingsEntry.java ir/SearchGUI.java
