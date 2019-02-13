207a30b923fe1a4f3606d957043c0b348beed465
Anton Lu <antolu@kth.se>
2019-02-13 16:26:12 +0100

    Added/fixed linux configurations

--- scripts/compile_all.sh
+++ scripts/compile_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if ! [ -d classes ];
+if ! [ -d build ];
 then
-   mkdir classes
+   mkdir build
 fi
-javac -cp . -d classes ir/Engine.java ir/Tokenizer.java ir/TokenTest.java ir/Index.java ir/Indexer.java ir/Searcher.java ir/HashedIndex.java ir/PersistentHashedIndex.java ir/Query.java ir/QueryType.java ir/RankingType.java ir/PostingsList.java ir/PostingsEntry.java ir/SearchGUI.java
+javac -cp . -d build ir/Engine.java ir/Tokenizer.java ir/TokenTest.java ir/Index.java ir/Indexer.java ir/Searcher.java ir/HashedIndex.java ir/PersistentHashedIndex.java ir/Query.java ir/QueryType.java ir/RankingType.java ir/PostingsList.java ir/PostingsEntry.java ir/SearchGUI.java ir/PersistentScalableHashedIndex.java
