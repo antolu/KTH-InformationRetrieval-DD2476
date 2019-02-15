1341f67532fdeb3493c41dcbf64f925ecae927bd
Anton Lu <antolu@kth.se>
2019-02-15 13:24:36 +0100

    Moved tokenIndex to Index, saves PostingsLists after retrieval

--- ir/PersistentHashedIndex.java
+++ ir/PersistentHashedIndex.java
@@ -45,8 +45,8 @@ public class PersistentHashedIndex implements Index {
     public static final String DOCINFO_FNAME = "docInfo";
 
     /** The dictionary hash table on disk can fit this many entries. */
-    public static final long TABLESIZE = 3509827L;
-    // public static final long TABLESIZE = 611953L;
+    // public static final long TABLESIZE = 3509s827L;
+    public static final long TABLESIZE = 611953L;
 
     /** Byte size of a long */
     protected static final int ENTRY_SIZE = 16;
@@ -69,7 +69,6 @@ public class PersistentHashedIndex implements Index {
 
     /** The cache as a main-memory hash map. */
     HashMap<String, PostingsList> index = new HashMap<String, PostingsList>();
-    HashMap<String, Integer> tokenIndex = new HashMap<>();
     protected int noUniqueTokens = 0;
 
     // ===================================================================
@@ -295,6 +294,9 @@ public class PersistentHashedIndex implements Index {
      * index.
      */
     public PostingsList getPostings(String token) {
+        if (index.containsKey(token))
+            return index.get(token);
+
         int hash = Utils.hash(token);
         int shash = Utils.reverseHash(token);
 
@@ -334,6 +336,8 @@ public class PersistentHashedIndex implements Index {
                 });
                 pl.add(postingsEntry);
             }
+
+            index.put(token, pl);
 
             return pl;
         } catch (NumberFormatException ex) {
