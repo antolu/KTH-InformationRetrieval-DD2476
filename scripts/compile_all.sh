18c3b81121516672962aeec770ab32bc2864d618
Anton Lu <antolu@kth.se>
2019-02-18 16:38:39 +0100

    Hotfix

--- scripts/compile_all.sh
+++ scripts/compile_all.sh
@@ -3,4 +3,4 @@ if ! [ -d build ];
 then
    mkdir build
 fi
-javac -cp . -d build ir/Engine.java ir/Tokenizer.java ir/TokenTest.java ir/Index.java ir/Indexer.java ir/Searcher.java ir/HashedIndex.java ir/PersistentHashedIndex.java ir/Query.java ir/QueryType.java ir/RankingType.java ir/PostingsList.java ir/PostingsEntry.java ir/SearchGUI.java ir/PersistentScalableHashedIndex.java
+javac -cp . -d build ir/*.java pagerank/*.java
--- scripts/run_pagerank.sh
+++ scripts/run_pagerank.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx1G -cp build PageRank pagerank/links1000.txt
+java -Xmx1G -cp build pagerank.PageRank pagerank/links1000.txt
