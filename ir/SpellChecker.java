901ac5441147c03f22eb8f3e0036f8aeef359331
Anton Lu <anton.lu@epfl.ch>
2019-03-20 15:35:32 +0100

    SpellChecker kinda working, needs better scoring, normalization

--- ir/SpellChecker.java
+++ ir/SpellChecker.java
@@ -7,7 +7,7 @@
 
 package ir;
 
-import java.util.List;
+import java.util.*;
 
 
 public class SpellChecker {
@@ -47,7 +47,6 @@ public class SpellChecker {
      */
     private static final double JACCARD_THRESHOLD = 0.4;
 
-
     /**
       * The threshold for edit distance for a candidate spelling
       * correction to be accepted.
@@ -66,10 +65,7 @@ public class SpellChecker {
      *  of the two sets contains <code>intersection</code> elements.
      */
     private double jaccard(int szA, int szB, int intersection) {
-        //
-        // YOUR CODE HERE
-        //
-        return 0;
+        return intersection * 1.0 / (szA + szB - intersection);
     }
 
     /**
@@ -80,11 +76,31 @@ public class SpellChecker {
      *      => substitute (cost 2)
      */
     private int editDistance(String s1, String s2) {
-        //
-        // YOUR CODE HERE
-        //
 
-        return -1;
+        int[][] m = new int[s1.length()][s2.length()];
+
+        for (int i = 1; i < s1.length(); i++) {
+            m[i][0] = i;
+        }
+        for (int i = 1; i < s2.length(); i++) {
+            m[0][i] = i;
+        }
+        for (int i = 1; i < s1.length(); i++) {
+            for (int j = 1; j < s2.length(); j++) {
+                int n = m[i-1][j-1];
+                if (s1.charAt(i) != s2.charAt(j)) n += 2;
+
+                n = Math.min(n, m[i-1][j] + 1);
+                n = Math.min(n, m[i][j-1] + 1);
+                m[i][j] = n;
+            }
+        }
+
+        return m[s1.length() - 1][s2.length() - 1];
+    }
+
+    private int getNoKgrams(String s, int K) {
+        return (int) Math.ceil((s.length() + 1.0) / K);
     }
 
     /**
@@ -92,11 +108,45 @@ public class SpellChecker {
      *  <code>limit</code> ranked suggestions for spelling correction.
      */
     public String[] check(Query query, int limit) {
-        //
-        // YOUR CODE HERE
-        //
+        String[] finalResults;
 
-        return null;
+        List<List<KGramStat>> unsortedResults = new ArrayList<>();
+
+        for (Query.QueryTerm qt: query.queryterm) {
+            Set<String> kgrams = KGramIndex.getKGrams(qt.term);
+
+            HashMap<String, KGramIndex.MutableInteger> words = kgIndex.getTokensFromKgrams(kgrams);
+            List<KGramStat> passJaccard = new ArrayList<>();
+
+            for (String kgramToken: words.keySet()) {
+                double jScore = jaccard(getNoKgrams(qt.term, kgIndex.K), kgIndex.numberOfKgrams.get(kgramToken), words.get(kgramToken).i);
+                if (jScore > JACCARD_THRESHOLD) {
+                    passJaccard.add(new KGramStat(kgramToken, jScore));
+                }
+            }
+
+            ArrayList<KGramStat> results = new ArrayList<>();
+            for (KGramStat kstat: passJaccard) {
+                int editDistance = editDistance(qt.term, kstat.token);
+                if (editDistance <= MAX_EDIT_DISTANCE) {
+                    kstat.score += editDistance / 2.0;
+                    results.add(kstat);
+                }
+            }
+
+            Collections.sort(results, Collections.reverseOrder());
+            unsortedResults.add(results);
+        }
+
+        List<KGramStat> sortedResults = mergeCorrections(unsortedResults, limit);
+
+        finalResults = new String[limit];
+
+        for (int i = 0; i < limit || i < sortedResults.size(); i++) {
+            finalResults[i] = sortedResults.get(i).token;
+        }
+
+        return finalResults;
     }
 
     /**
@@ -105,9 +155,17 @@ public class SpellChecker {
      *  to <code>limit</code> corrected phrases.
      */
     private List<KGramStat> mergeCorrections(List<List<KGramStat>> qCorrections, int limit) {
-        //
-        // YOUR CODE HERE
-        //
-        return null;
+        List<KGramStat> results = new ArrayList<>();
+
+        for (List<KGramStat> tknList: qCorrections) {
+            results.addAll(tknList);
+        }
+
+        Collections.sort(results, Collections.reverseOrder());
+
+        if (results.size() > limit)
+            return results.subList(0, limit);
+        else
+            return results;
     }
 }
